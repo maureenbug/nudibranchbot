082369355eec0354b044e0021003d309f7131054
Maureen Berg <maureenberg@Maureens-MacBook-Pro.local>
2017-06-19 20:19:24 -0700

    second commit

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {
+  consumer_key:         'E99HF4u4mbhiCkw82YZGLttlk',
+  consumer_secret:      'edJQLe3PhQ9IglPlV1AFbsbEJcA7nRWFbQ3p6XeF52URVtUIsv',
+  access_token:         '876979356202958849-xMhwqvBJu0vusIrITRYw5wZyXgbNTBK',
+  access_token_secret:  'CT5gRDXktPSbDslD9UQB3c3zLRwwbMDfjW6Ow0qPJfW4c'
+}
+
+module.exports = config;
